f98b277717a729f3f37fdfd871bed8c0630cfd48
Argus!Cromwel <kelvin21706@gmail.com>
2023-05-20 11:45:26 +0800

    added tests

---
+++ test.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const { faker } = require('@faker-js/faker');
+const Article = require('./models/article');
+const config = require('./config');
+
+// Connect to the database
+mongoose.connect('mongodb://' + config.Mongodb.host + '/' + config.Mongodb.name, {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+}).then(() =>
+        console.log("Connected to MongoDB")
+).catch(() =>
+    console.log("Error connecting to MongoDB, is the service Online?")
+)
+// Generate 100 sample articles
+for (let i = 0; i < 100; i++) {
+  // Generate a random title
+  const title = faker.lorem.words(10);
+
+  // Generate a random description
+  const description = faker.lorem.words(10);
+
+  // Generate a random markdown content
+  const markdown = faker.lorem.paragraphs(10);
+  const author = faker.person.fullName();
+  const hashtags = faker.lorem.words(6);
+
+  // Create a new article with the generated data
+  const article = new Article({
+    title: title,
+    description: description,
+    markdown: markdown,
+    type: 'English',
+    author: author,
+    hashtags: hashtags
+  });
+
+  // Save the article to the database
+  article.save()
+    .then(() => console.log(`Saved article ${i+1}`))
+    .catch(err => console.error(err));
+}
